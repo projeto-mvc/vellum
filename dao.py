f3446de514d97ddaa8b13a62e6f207eeca1d4431
JohnWayne001 <johnwaynegtmelo@gmail.com>
2026-05-13 16:35:56 -0300

    Delete dao.py

--- dao.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def connection():
-     return mysql.connector.connect(
-          host="localhost",
-          user="root",
-          password="",
-          database="lista_chamada"
-     )
